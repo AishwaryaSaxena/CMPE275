cad1693de4342f1cca6f7ccf9373b429dc20a899
Phani Kantamneni <teja.kantamneni@outlook.com>
2018-11-12 03:12:07 -0800

    updated raft algortihm to work with data center

--- raft_implementation_grpc/raft.py
+++ raft_implementation_grpc/raft.py
@@ -17,7 +17,7 @@ class States(Enum):
     Leader = 3
 
 my_id = "localhost:4000"
-my_vote = False7
+my_vote = False
 my_term = 1
 my_state = States.Follower
 delay = uniform(1.0, 1.5)
@@ -26,32 +26,28 @@ friends = ["localhost:4001", "localhost:4002"]
 hb_recv = False
 vr_recv = False
 # friends = ["localhost:4001", "localhost:4002", "localhost:4003", "localhost:4004"]
-dcs = ["localhost:5000", "localhost:5001"]
+dcs = ["localhost:5000", "localhost:5001", "localhost:5002", "localhost:5003", "localhost:5004"]
 dc_files = {}
 file_max_chunks = {}
 file_log = {}
 
 class RaftImpl(raft_pb2_grpc.raftImplemetationServicer, file_transfer_pb2_grpc.DataTransferServiceServicer):
     def RequestVote(self, voteReq, context):
-        global vr_recv, my_state, my_term, hb_recv
-        # vr_recv = True
-        # if my_term > voteReq.currentTerm or my_state == States.Leader:
-        #     return Vote(voted=False, currentTerm=my_term)
-        # else:
-        #     if my_vote and my_term == voteReq.currentTerm:
-        #         return Vote(voted=False, currentTerm=my_term)
-        #     else:
-        #         my_term = voteReq.currentTerm
-        #         return Vote(voted=True, currentTerm=my_term)
+        global vr_recv, my_state, my_term, hb_recv, my_vote
         if hb_recv == True:
             return Vote(voted=False, currentTerm=my_term)
         else:
-            if my_term >= voteReq.currentTerm or my_state == States.Leader:
+            if my_term > voteReq.currentTerm or my_state == States.Leader:
                 return Vote(voted=False, currentTerm=my_term)
             else:
-                vr_recv = True
-                my_term = voteReq.currentTerm
-                return Vote(voted=True, currentTerm=my_term)
+                if my_term > voteReq.currentTerm or my_state == States.Leader:
+                    return Vote(voted=False, currentTerm=my_term)
+                else:
+                    if my_vote and my_term == voteReq.currentTerm:
+                        return Vote(voted=False, currentTerm=my_term)
+                    else:
+                        my_term = voteReq.currentTerm
+                        return Vote(voted=True, currentTerm=my_term)
 
     def AppendEntries(self):
         pass
@@ -112,6 +108,7 @@ def client():
             timer()
             if not hb_recv and not vr_recv:
                 if my_state == States.Follower:
+                    #print("in client -> while -> else -> if -> if")
                     my_state = States.Candidate
                     my_term += 1
                     my_vote = True
@@ -119,25 +116,31 @@ def client():
                 else:
                     my_term += 1
                     my_vote = True
+                    #print("in client -> while -> else -> if -> else")
                     startElection()
 
 
 def timer():
     global delay, hb_recv, vr_recv, my_term, my_state
     if my_term == 1:
-        sleep(uniform(2.0, 3.0))
+        #print("in timer -> if")
+        sleep(uniform(1.0, 1.5))
     else:
         while True:
             print("Waiting for heartbeat", my_state.name, my_term)
             print(file_log, file_max_chunks)
+            #print("in timer -> else -> while")
             if hb_recv:
                 hb_recv = False
-                sleep(uniform(2.0, 3.0))
+                #print("in timer -> else -> while -> if")
+                sleep(uniform(1.0, 1.5))
             elif vr_recv:
                 vr_recv = False
-                sleep(uniform(2.0, 3.0))
+                #print("in timer -> else -> while -> elif")
+                sleep(uniform(1.0, 1.5))
             else:
-                sleep(uniform(2.0, 3.0))
+                #print("in timer -> else -> while -> else")
+                sleep(uniform(1.0, 1.5))
                 break
 
 def startElection():
@@ -145,18 +148,24 @@ def startElection():
     vote_count = 1
     vote = ""
     stubs = []
+    #print("in startElection")
     for friend in friends:
         stubs.append(raft_pb2_grpc.raftImplemetationStub(grpc.insecure_channel(friend)))
     for stub in stubs:
         try:
+            #print("in startElection RequestVote stub call")
             vote = stub.RequestVote(VoteReq(id=my_id, currentTerm=my_term), timeout=0.1)
             if vote.voted:
+                #print("in startElection -> for -> try -> if")
                 vote_count += 1
             else:
+                #print("in startElection -> for -> try -> else")
                 my_term = vote.currentTerm
         except:
-            print("node dead")
-    print("votes received: ", vote_count)
+            #print("in startElection -> for -> except")
+            #print("node dead")
+            pass
+    #print("votes received: ", vote_count)
     if vote_count > (len(stubs)+1)//2:
         my_state = States.Leader
         return
@@ -169,16 +178,22 @@ def leaderActions():
     hb_ack = ""
     while my_state == States.Leader:
         print("Sending Heartbeats", my_state.name, my_term)
-        print(file_log, file_max_chunks)
+        #print(file_log, file_max_chunks)
+        #print("in leaderActions -> while")
         stub = ""
         for friend in friends:
             stub = raft_pb2_grpc.raftImplemetationStub(grpc.insecure_channel(friend))
             try:
+                d = {}
                 for file_key in file_log.keys(): 
-                    file_log[file_key] = DcList(dcs=file_log[file_key])
-
-                hb_ack = stub.SendHeartBeat(Heartbeat(id=my_id, currentTerm=my_term, log = Log(fileLog = file_log, maxChunks = file_max_chunks)), timeout=0.1)
-            except:
+                    d[file_key] = DcList(dcs=file_log[file_key])
+                #print("in leaderActions -> while -> for -> try")
+                hb_ack = stub.SendHeartBeat(Heartbeat(id=my_id, currentTerm=my_term, log = Log(fileLog = d, maxChunks = file_max_chunks)), timeout=0.1)
+                #hb_ack = stub.SendHeartBeat(Heartbeat(id=my_id, currentTerm=my_term))
+                #print(hb_ack.ack)
+            except Exception as e:
+                #print(e.__class__.__name__)
+                #print("in leaderActions -> while -> for -> except")
                 #print("node dead")
                 pass
         sleep(0.5)
@@ -187,18 +202,18 @@ def checkDcHealth():
     global my_state, dcs, my_id, dc_files, file_max_chunks, file_log
     while True:
         if my_state == States.Leader:
-            # stub = ""
+            #stub = ""
             for dc in dcs:
                 stub = raft_pb2_grpc.raftImplemetationStub(grpc.insecure_channel(dc))
                 try:
-                    hb_ack = stub.SendHeartBeat(Heartbeat(id=my_id))
-                    dc_ack = hb_ack.dcAck
-                    max_chunks = hb_ack.maxChunks
+                    hb_ack = stub.SendHeartBeat(Heartbeat(id=my_id), timeout=0.1)
+                    dc_ack = list(hb_ack.dcAck)
+                    max_chunks = dict(hb_ack.maxChunks)
                     for f in max_chunks.keys():
                         if f not in file_max_chunks.keys():
                             file_max_chunks[f] = max_chunks[f]
                     dc_files[dc] = list(set([f.rsplit('_', 1)[0] for f in dc_ack]))
-                    for f_c in list(set(list(dc_ack) + list(file_log.keys()))):
+                    for f_c in list(set(dc_ack + list(file_log.keys()))):
                         if f_c not in file_log.keys():
                             file_log[f_c] = [dc]
                         else:
@@ -209,10 +224,11 @@ def checkDcHealth():
                                 if dc in file_log[f_c]:
                                     file_log[f_c].remove(dc)
 
-                    print(dc_files, "\n", file_max_chunks, "\n", file_log)
+                    #print(dc_files, "\n", file_max_chunks, "\n", file_log)
                 except:
-                    print("node dead")
+                    print("dc dead")
         sleep(5)
+
 
 if __name__ == '__main__':
     t1 = Thread(target=serve)
@@ -222,3 +238,4 @@ if __name__ == '__main__':
     t1.start()
     t2.start()
     t3.start()
+    
